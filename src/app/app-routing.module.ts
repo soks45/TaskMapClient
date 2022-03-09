4e597b13b58d802234d08ba7a7d6d1e6f6f13b45
Aleksey Batuev <abatuev@cloudlinux.com>
2022-03-09 17:19:23 +0500

    added main-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'main-page',
+    loadChildren: () => import('./pages/main-page/main-page.module').then(m => m.MainPageModule),
+    // canActivate: [true],
+  },
+  {
+    path: '**',
+    redirectTo: 'main-page'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(
+      routes,
+      { preloadingStrategy: PreloadAllModules, relativeLinkResolution: 'legacy' },
+    ),
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }

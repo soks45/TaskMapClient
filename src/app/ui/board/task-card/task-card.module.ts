4b3e7b5dafec02d4878ff0f4cd4a44405f1ff8b1
Danil Khabibullin <dkhabibullin@cloudlinux.com>
2022-04-18 15:26:19 +0500

    Change card style

--- src/app/ui/board/task-card/task-card.module.ts
+++ src/app/ui/board/task-card/task-card.module.ts
@@ -4,6 +4,8 @@ import { TaskCardComponent } from 'src/app/ui/board/task-card/task-card.componen
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from "@angular/material/icon";
+
 
 
 
@@ -14,7 +16,8 @@ import { MatButtonModule } from '@angular/material/button';
     CommonModule,
     MatCardModule,
     MatButtonModule,
-    DragDropModule
+    DragDropModule,
+    MatIconModule
   ]
 })
 export class TaskCardModule { }

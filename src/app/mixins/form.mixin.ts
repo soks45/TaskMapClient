7616f21ee81fb66cba7cec51e3ffe45b48fbfca2
soks <soks145@gmail.com>
2022-09-24 03:46:47 +0500

    removed unused methods.

--- src/app/mixins/form.mixin.ts
+++ src/app/mixins/form.mixin.ts
@@ -1,5 +1,5 @@
 import { Directive } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, ValidationErrors } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 import { Constructor } from '@mixins/mixins';
 
 export type ControlsOf<T extends Record<string, any>> = {
@@ -24,34 +24,6 @@ export function FormMixin<TBase extends Constructor, T extends Record<string, an
 
         protected markFormGroupTouched(): void {
             this.formGroup.markAsTouched();
-        }
-
-        protected validatePasswords(control: AbstractControl, name: string): ValidationErrors | null {
-            if (!this.password1 || !this.password2) {
-                return null;
-            }
-            if (this.formGroup === undefined || this.password1.value === '' || this.password2.value === '') {
-                return null;
-            } else if (this.password1.value === this.password2.value) {
-                if (name === 'password' && this.password2.hasError('passwordMismatch')) {
-                    this.password1.setErrors(null);
-                    this.password2.updateValueAndValidity();
-                } else if (name === 'passwordConfirm' && this.password1.hasError('passwordMismatch')) {
-                    this.password2.setErrors(null);
-                    this.password1.updateValueAndValidity();
-                }
-                return null;
-            } else {
-                return { passwordMismatch: { value: 'The provided passwords do not match' } };
-            }
-        }
-
-        get password1(): AbstractControl | null {
-            return this.formGroup.get('password');
-        }
-
-        get password2(): AbstractControl | null {
-            return this.formGroup.get('passwordConfirm');
         }
     }
 

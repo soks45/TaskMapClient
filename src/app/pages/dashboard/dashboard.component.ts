5f88c7a4f932aa56a93f52aa1e6a9ab9c3152c7c
soks <soks145@gmail.com>
2023-03-17 15:48:59 +0500

    added drag and drop for tasks dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,15 @@
-import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
 import { Board } from '@models/board';
 import { TaskB } from '@models/task-b';
 import { BoardService } from '@services/board/board.service';
 import { TaskService } from '@services/task/task.service';
-import { Observable } from 'rxjs';
+import { combineLatest, Observable, switchMap } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+interface BoardWithTasks extends Board {
+    tasks: TaskB[];
+}
 
 @Component({
     selector: 'tm-dashboard',
@@ -12,15 +17,37 @@ import { Observable } from 'rxjs';
     styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
-    boards$?: Observable<Board[]>;
+    boards$?: Observable<BoardWithTasks[]>;
 
-    constructor(private boards: BoardService, public tasks: TaskService) {}
+    constructor(private boardService: BoardService, private tasks: TaskService) {}
 
     ngOnInit(): void {
-        this.boards$ = this.boards.get();
+        this.boards$ = this.boardService.get().pipe(
+            switchMap((boards) =>
+                combineLatest(
+                    boards.map((board) =>
+                        this.tasks.get(board.boardId).pipe(
+                            map((tasks) => ({
+                                ...board,
+                                tasks,
+                            }))
+                        )
+                    )
+                )
+            )
+        );
     }
 
-    drop($event: CdkDragDrop<TaskB[]>, tasks: TaskB[]) {
-        moveItemInArray(tasks, $event.previousIndex, $event.currentIndex);
+    drop($event: CdkDragDrop<TaskB[]>) {
+        if ($event.previousContainer === $event.container) {
+            moveItemInArray($event.container.data, $event.previousIndex, $event.currentIndex);
+        } else {
+            transferArrayItem(
+                $event.previousContainer.data,
+                $event.container.data,
+                $event.previousIndex,
+                $event.currentIndex
+            );
+        }
     }
 }

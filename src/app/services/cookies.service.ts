94abe29aadfac2bbf27ec058ea68eb9091b12ace
Aleksey Batuev <abatuev@cloudlinux.com>
2023-05-21 15:57:56 +0500

    lint-formatted

--- src/app/services/cookies.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { CookieService } from 'ngx-cookie-service';
-
-@Injectable({
-    providedIn: 'root',
-})
-export class CookiesService {
-    constructor(private cookieService: CookieService) {}
-}

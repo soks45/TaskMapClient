c767839b3d3fa60d06031c041c519099534a77a3
Aleksey Batuev <abatuev@cloudlinux.com>
2023-06-10 23:05:54 +0500

    added header interceptor

---
+++ src/app/interceptors/board-group.interceptor.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpHeaders } from '@angular/common/http';
+import { CurrentBoardDataSource } from '@services/data-sources/current-board.data-source';
+import { Board } from 'app/models/board';
+import { Observable, switchMap } from 'rxjs';
+
+export const NEED_BOARD_GROUP = 'Board-Group';
+
+export function boardGroupHeader(headers?: HttpHeaders): HttpHeaders {
+    if (!headers) {
+        headers = new HttpHeaders();
+    }
+
+    return headers.set(NEED_BOARD_GROUP, NEED_BOARD_GROUP);
+}
+
+@Injectable()
+export class BoardGroupInterceptor implements HttpInterceptor {
+    constructor(private currentBoardService: CurrentBoardDataSource) {}
+
+    intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+        if (request.headers.has(NEED_BOARD_GROUP)) {
+            return this.currentBoardService.lastValue().pipe(
+                switchMap((currentBoard: Board) =>
+                    next.handle(
+                        request.clone({
+                            setHeaders: { [NEED_BOARD_GROUP]: currentBoard.boardId.toString() },
+                        })
+                    )
+                )
+            );
+        }
+
+        return next.handle(request);
+    }
+}
